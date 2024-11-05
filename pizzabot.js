209f9006e3cbceaee5b7099a9d68be183b4922a6
Noel Perland <Noel.Perland@yh.nackademin.se>
2024-11-05 16:42:55 +0100

    task 3 done. Time to clean up the code with task 4

--- pizzabot.js
+++ pizzabot.js
@@ -1,5 +1,5 @@
 //task 1.1
-const vegetarian = "vegerarian pizza";
+const vegetarian = "vegetarian pizza";
 const hawaiian = "hawaiian pizza";
 const pepperoni = "pepperoni pizza";
 
@@ -16,14 +16,37 @@ alert(
 
 //task 2.3
 const orderName = prompt("Enter the name of the pizza you want to order");
-const orderQuantity = prompt("How mant of the " + orderName + " do you want?");
+const orderQuantity = prompt("How many of the " + orderName + " do you want?");
 
-//task 2.4
-const orderTotal = orderQuantity * pizzaPrice;
-alert(
-  "Fantastic choice. I will get started or your " +
-    orderName +
-    " right away. Your final will be " +
-    orderTotal +
-    ":-"
-);
+//task 2.4 flyttade till if-satsen i task 3.1
+
+//task 3.1
+if (orderName === vegetarian || orderName === hawaiian || pepperoni) {
+  const orderQuantity = parseInt(
+    prompt("how many of " + orderName + " do you want?"),
+    10
+  );
+  //task 2.4.2
+  const orderTotal = orderQuantity * pizzaPrice;
+
+  //task 3.2
+  let cookingTime;
+  if (orderQuantity >= 1 && orderQuantity <= 2) {
+    cookingTime = 10;
+  } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+    cookingTime = 15;
+  } else {
+    cookingTime = 20;
+  }
+  //task 3.3
+
+  alert(
+    "Fantastic choice. I will get started on your " +
+      orderName +
+      " right away. Your final total will be " +
+      orderTotal +
+      ":-"
+  );
+} else {
+  alert("Sorry, we don't have that pizza on our menu.");
+}
