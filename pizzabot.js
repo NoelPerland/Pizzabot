01c2ac1d76f64cbc441d49d76ea68207b97c63de
Noel Perland <Noel.Perland@yh.nackademin.se>
2024-11-05 17:47:19 +0100

    Tidied up the code and tested it and  it runs correctly. All done up until  theStretch Goals - HTML user interface .

--- pizzabot.js
+++ pizzabot.js
@@ -1,52 +1,78 @@
-//task 1.1
+// The most important variables
 const vegetarian = "vegetarian pizza";
 const hawaiian = "hawaiian pizza";
 const pepperoni = "pepperoni pizza";
-
 const pizzaPrice = 80; // no inflation
-//task 1.2
-alert(
-  "Hi! Welcome to Pizzabot. On our menu we have " +
-    vegetarian +
-    ", " +
-    hawaiian +
-    " and " +
-    pepperoni
+
+//Func to greet the costumer
+function greetCostumer() {
+  alert(
+    "Hi! Welcome to Pizzabot. On our menu we have " +
+      vegetarian +
+      ", " +
+      hawaiian +
+      " and " +
+      pepperoni
+  );
+}
+
+//Func to check if the pizza is on the menu
+function checkOrderName(orderName) {
+  const lowerCaseOrder = orderName.toLowerCase(); // tror detta gör det simplare
+  return (
+    lowerCaseOrder === vegetarian ||
+    lowerCaseOrder === hawaiian ||
+    lowerCaseOrder === pepperoni
+  );
+}
+
+//Func to check the total price of the order
+function totalCost(orderQuantity) {
+  return orderQuantity * pizzaPrice;
+}
+
+//Func to check cooking time
+function cookingTime(orderQuantity) {
+  if (orderQuantity >= 1 && orderQuantity <= 2) {
+    return 10;
+  } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+    return 15;
+  } else {
+    return 20;
+  }
+}
+
+// Main program flow
+greetCostumer();
+
+const orderName = prompt(
+  "Enter the name of the pizza you want to order today."
 );
 
-//task 2.3
-const orderName = prompt("Enter the name of the pizza you want to order");
-const orderQuantity = prompt("How many of the " + orderName + " do you want?");
-
-//task 2.4 flyttade till if-satsen i task 3.1
-
-//task 3.1
-if (orderName === vegetarian || orderName === hawaiian || pepperoni) {
+if (checkOrderName(orderName)) {
   const orderQuantity = parseInt(
-    prompt("how many of " + orderName + " do you want?"),
+    prompt("How many of " + orderName + " do you want?"),
     10
   );
-  //task 2.4.2
-  const orderTotal = orderQuantity * pizzaPrice;
 
-  //task 3.2
-  let cookingTime;
-  if (orderQuantity >= 1 && orderQuantity <= 2) {
-    cookingTime = 10;
-  } else if (orderQuantity >= 3 && orderQuantity <= 5) {
-    cookingTime = 15;
+  // Check if orderQuantity is valid
+  if (isNaN(orderQuantity) || orderQuantity <= 0) {
+    alert("You can't order zero pizzas you jerk.");
   } else {
-    cookingTime = 20;
+    // Calculate order total and cooking time
+    const orderTotal = totalCost(orderQuantity);
+    const orderCookingTime = cookingTime(orderQuantity);
+    alert(
+      "Fantastic choice. I will get started on your " +
+        orderName +
+        " right away. " +
+        "Your final total will be " +
+        orderTotal +
+        ":-. The pizzas will take " +
+        orderCookingTime +
+        " minutes."
+    );
   }
-  //task 3.3
-
-  alert(
-    "Fantastic choice. I will get started on your " +
-      orderName +
-      " right away. Your total will be " +
-      orderTotal +
-      ":-"
-  );
 } else {
   alert("Sorry, we don't have that pizza on our menu.");
 }
